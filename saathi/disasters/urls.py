3d0d5c7730dc291b4164a73f66a3e94add1952b8
bawabanna230 <chirag,agarwal.ece17@itbhu.ac.in>
2018-10-24 23:28:43 +0530

    Home page fixed

--- saathi/disasters/urls.py
+++ saathi/disasters/urls.py
@@ -6,6 +6,6 @@ app_name='chat'
 urlpatterns = [
     path('kerala_floods/', views.Kerala_FloodsView, name='kerala_floods'),
     path('cyclone_titli/', views.Cyclone_TitliView, name='cyclone_titli'),
-    path('indonesian_tsunami/', views.Indonesian_TsunamiView, name='Indonesian_Tsunami'),
-    path('hurricane_michael/', views.Hurricane_MichaelView, name='Hurricane_Michael'),
+    path('indonesian_tsunami/', views.Indonesian_TsunamiView, name='indonesian_tsunami'),
+    path('hurricane_michael/', views.Hurricane_MichaelView, name='hurricane_michael'),
 ]
